9c8c91ac6ddfc877e495757bc0d1e075d16de79c
ydejene <y.dejene@alustudent.com>
2025-10-31 19:12:38 +0300

    trial 4

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,3 +18,21 @@ if __name__ == "__main__":
         params={"_expand": "user"} 
     )
     data = response.json()
+
+    if response.status_code == 404:
+        print("RequestError:", 404)
+        sys.exit(1)
+    elif not data:
+        print("No tasks found for that employee ID.")
+        sys.exit(1)
+
+    EMPLOYEE_NAME = data[0].get("user", {}).get("name")
+    done_tasks = [task for task in data if task.get("completed")]
+    NUMBER_OF_DONE_TASKS = len(done_tasks)
+    TOTAL_NUMBER_OF_TASKS = len(data)
+
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for task in done_tasks:
+        TASK_TITLE = task.get("title")
+        print(f"\t {TASK_TITLE}")
