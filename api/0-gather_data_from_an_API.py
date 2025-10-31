a2aa7c7c3a92d46b4661659bfbb59cc3dc62884e
ydejene <y.dejene@alustudent.com>
2025-10-31 18:52:37 +0300

    trial 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,3 @@
 #!/usr/bin/python3
 """Script to use a REST API for a given employee ID, returns
 information about his/her TODO list progress"""
-
-import requests
-import sys
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} missing employee_id(int)")
-        sys.exit(1)
-
-    API_URL = "https://jsonplaceholder.typicode.com"
-    EMPLOYEE_ID = sys.argv[1]
-
-    response = requests.get(
-        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
-        params={"_expand": "user"} 
-    )
-    data = response.json()
-
-    if response.status_code == 404:
-        print("RequestError:", 404)
-        sys.exit(1)
-    elif not data:
-        print("No tasks found for that employee ID.")
-        sys.exit(1)
-
-    employee_name = data[0].get("user", {}).get("name")
-    total_tasks = len(data)
-    done_tasks = [task for task in data if task.get("completed")]
-    total_done_tasks = len(done_tasks)
-
-    print(f"Employee {employee_name} is done with tasks"
-          f"({total_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task.get("title")}")
